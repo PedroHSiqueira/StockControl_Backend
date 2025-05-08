a5f65644a9906194d30fb6317a398b6f96f8e443
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-08 19:00:04 -0300

    Feat: Adiciona Convites

--- src/routes/convites/delete-convites.ts
+++ src/routes/convites/delete-convites.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../../lib/prisma";
 
 export async function deleteConvite(app: FastifyInstance) {
-  app.delete("/notificacao/:id", async (request, reply) => {
+  app.delete("/convite/:id", async (request, reply) => {
     const { id } = request.params as { id: string };
 
     const convite = await prisma.convite.delete({
