9dee7898c57f9619218518c2fee175109f651141
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-10 20:10:01 -0300

    Feat: contagem de itens na dashboard

--- src/routes/produtos/get-produtos.ts
+++ src/routes/produtos/get-produtos.ts
@@ -13,5 +13,19 @@ export async function getProduto(app: FastifyInstance) {
 
     reply.send(fornecedor);
   });
+
+  app.get("/produtos/contagem", async (request, reply) => {
+    const produtos = await prisma.produto.findMany({
+      select: {
+        quantidade: true,
+        preco: true,
+      },
+    });
+
+    const contagemQuantidade = produtos.reduce((sum, produto) => sum + (produto.quantidade || 0), 0);
+    const contagemPreco = produtos.reduce((sum, produto) => sum + (produto.preco || 0) * (produto.quantidade || 0), 0);
+
+    reply.send({ contagemQuantidade, contagemPreco });
+  });
 }
 
