3ef1bf2754d879e8c00184165155e68cc8d154a1
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-05-21 22:05:07 -0300

    fix: notificacoes

--- src/server.ts
+++ src/server.ts
@@ -42,7 +42,7 @@ const app = fastify();
 
 // Configuração do CORS
 app.register(fastifyCors, {
-  origin: ["http://localhost:3000"],
+  origin: ["https://stockcontrol-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "user-id", "client_key"],
   credentials: true,
