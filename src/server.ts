9bb2f6b19b427d09658b2941cc57040f01cee75f
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-08-28 14:42:20 -0300

    feat: ajustes catalogo

--- src/server.ts
+++ src/server.ts
@@ -48,7 +48,7 @@ const app = fastify();
 
 // Configuração do CORS
 app.register(fastifyCors, {
-  origin: ["http://localhost:3000"],
+  origin: ["https://stockcontrolsix-vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "user-id", "client_key", "Accept"],
   credentials: true,
