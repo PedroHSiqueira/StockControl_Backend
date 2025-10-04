9fb458e453ce496ada79ac2c0d079399484ef16c
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-10-04 15:56:23 -0300

    Feat: adicionando Paginação as Logs

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const app = fastify({
 });
 
 app.register(fastifyCors, {
-  origin: ["http://localhost:3000"],
+  origin: ["https://stockcontrol-six.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "user-id", "client_key", "Accept"],
   credentials: true,
