d128b9950559a9d53d56cb63b5f4f6c948ed4304
Pedro Mendes Lima <pedromendeslima2016@gmail.com>
2025-10-10 22:08:30 -0300

    fix: jwt verificar email empresa

--- src/routes/empresas/get-empresa.ts
+++ src/routes/empresas/get-empresa.ts
@@ -41,9 +41,6 @@ export async function getEmpresa(app: FastifyInstance) {
 
   app.get("/empresa/verificar-email/:email", async (request: FastifyRequest, reply: FastifyReply) => {
     try {
-      await request.jwtVerify().catch(() => {
-        throw new UnauthorizedError("Token inválido ou expirado");
-      });
       const { email } = request.params as { email: string };
 
       const url = request.url;
@@ -95,10 +92,6 @@ export async function getEmpresa(app: FastifyInstance) {
         mensagem: mensagem,
       });
     } catch (error) {
-      if (error instanceof UnauthorizedError) {
-        return reply.status(401).send({ error: error.message });
-      }
-
       return reply.status(500).send({ mensagem: "Erro interno no servidor" });
     }
   });
